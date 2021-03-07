5a3b9eac1efb9923bdc496d6f61c85670f6165e9
jgarces <jgarcesembarba@gmail.com>
2021-03-07 08:51:56 +0100

    chore: added mysql basic docker config

---
+++ docker/mysql/blog_db.sql
@@ -0,0 +1,19 @@
+create DATABASE IF NOT EXISTS blog_db;
+
+USE blog_db;
+
+# Volcado tabla authors
+# -------------------------------------
+DROP TABLE IF EXISTS `authors`;
+CREATE TABLE `authors` (
+    `id` varchar(36) NOT NULL,
+    `name` varchar(36) NOT NULL,
+    `alias` varchar(36) NOT NULL,
+    `contact_email` varchar(256) NOT NULL,
+    `personal_description` text,
+    `short_description` text NOT NULL,
+    `avatar` varchar(256) NOT NULL,
+    `social_media` text,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=urf8mb4;
+
