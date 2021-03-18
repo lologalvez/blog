1838aa6b125c7724eddc5bb2b7ed430d06ead806
jgarces <jgarcesembarba@gmail.com>
2021-03-18 10:18:33 +0100

    feat: added short description value object

---
+++ src/Domain/Model/Author/Data/ShortDescription.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Model\Author\Data;
+
+class ShortDescription
+{
+    private const MAX_LENGTH = 275;
+
+    private string $shortDescription;
+
+    public function __construct(string $shortDescription)
+    {
+        $this->setShortDescription($shortDescription);
+    }
+
+    private function setShortDescription(string $shortDescription)
+    {
+        $this->validate($shortDescription);
+        $this->shortDescription = $shortDescription;
+    }
+
+    private function validate(string $shortDescription)
+    {
+        if (strlen($shortDescription) > self::MAX_LENGTH) {
+            throw new InvalidAuthorDataException(
+                sprintf("Short Description should be less than %s characters", self::MAX_LENGTH)
+            );
+        }
+    }
+}
---
+++ tests/unit/Domain/Model/Author/Data/ShortDescriptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\unit\Domain\Model\Author\Data;
+
+use App\Domain\Model\Author\Data\InvalidAuthorDataException;
+use App\Domain\Model\Author\Data\ShortDescription;
+use PHPUnit\Framework\TestCase;
+
+class ShortDescriptionTest extends TestCase
+{
+    /** @test */
+    public function should_not_allow_more_than_275_characters(): void
+    {
+        $this->expectException(InvalidAuthorDataException::class);
+        $this->expectExceptionMessage('Short Description should be less than 275 characters');
+        new ShortDescription('Morbi maximus, libero quis auctor venenatis, ligula metus condimentum enim, eget convallis neque mauris et leo. Cras porttitor metus sit amet lectus porttitor lacinia. Duis pulvinar, risus in aliquet imperdiet, diam magna convallis eros, sagittis gravida libero leo a quam le.');
+    }
+}
