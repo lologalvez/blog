4f9323933bf5ab57fb7927094ad847a8a2161937
jgarces <jgarcesembarba@gmail.com>
2021-04-20 22:48:38 +0200

    feat: edit author service throws exception if author id is not found

--- tests/unit/Application/Author/EditAuthorTest.php
+++ tests/unit/Application/Author/EditAuthorTest.php
@@ -5,21 +5,31 @@ declare(strict_types=1);
 namespace App\Tests\unit\Application\Author;
 
 use App\Application\Author\EditAuthor;
+use App\Domain\Model\Author\AuthorNotFoundException;
 use App\Domain\Model\Author\AuthorRepository;
 use App\Domain\Model\Id\Id;
 use App\Tests\unit\Domain\Model\Author\AuthorBuilder;
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
 
 class EditAuthorTest extends TestCase
 {
+    use ProphecyTrait;
+
     private const AUTHOR_ID = '0d67e364-813b-42a2-ba3f-20a86fc2c274';
+
+    private $authorRepository;
+    private $editAuthor;
+
+    protected function setUp(): void
+    {
+        $this->authorRepository = $this->prophesize(AuthorRepository::class);
+        $this->editAuthor = new EditAuthor($this->authorRepository->reveal());
+    }
 
     /** @test */
     public function should_edit_an_authors_information(): void
     {
-        $authorRepository = $this->prophesize(AuthorRepository::class);
-        $editAuthor = new EditAuthor($authorRepository->reveal());
-
         $anAuthorId = new Id(self::AUTHOR_ID);
         $authorData = [
             'id' => self::AUTHOR_ID,
@@ -33,10 +43,10 @@ class EditAuthorTest extends TestCase
                 'instagram' => 'an instagram link',
             ],
         ];
-        $authorRepository->findById($anAuthorId)
+        $this->authorRepository->findById($anAuthorId)
             ->willReturn(AuthorBuilder::anAuthor()->build());
 
-        $editAuthor->execute($authorData);
+        $this->editAuthor->execute($authorData);
 
         $expectedAuthor = AuthorBuilder::anAuthor()
             ->withId($anAuthorId)
@@ -48,6 +58,16 @@ class EditAuthorTest extends TestCase
             ->withAvatar('an avatar')
             ->withSocialMediaLinks(['instagram' => 'an instagram link'])
             ->build();
-        $authorRepository->edit($anAuthorId, $expectedAuthor)->shouldHaveBeenCalled();
+        $this->authorRepository->edit($anAuthorId, $expectedAuthor)->shouldHaveBeenCalled();
+    }
+
+    /** @test */
+    public function should_throw_exception_if_author_id_does_not_exist(): void
+    {
+        $authorData = ['id' => self::AUTHOR_ID];
+        $this->authorRepository->findById(new Id(self::AUTHOR_ID))->willReturn(null);
+
+        $this->expectException(AuthorNotFoundException::class);
+        $this->editAuthor->execute($authorData);
     }
 }
